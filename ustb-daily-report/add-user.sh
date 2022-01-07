c76ed6caa5a4b150fcaccf19f4489648c6408a15
terrance <tailin.liang@outlook.com>
2022-01-07 20:16:02 +0800

    multi area support

--- ustb-daily-report/add-user.sh
+++ ustb-daily-report/add-user.sh
@@ -4,13 +4,25 @@ export HASS_HOME=/volume1/docker/hass
 export SCRIPT_HOME=$HASS_HOME/ustb-daily-report/
 export REPORT_APP=$HASS_HOME/ustb-daily-report/ustb-report
 export DIV='=============================================================================='
+export WWW_DIR="/volume1/mount/home/www/ustb-log"
+export LOG_FILE="$SCRIPT_HOME/manage.log"
+export KEEP_ALIVE_LINE=8
+# recevie data from CLI
+INPUT_CMD=$1
+USER_NAME=$2
+USER_AGENT=$3
+COOKIE=$4
+DATA=$5
+USER_LOC=$6
+USER_LATI=$7
+USER_LONG=$8
 
-WWW_DIR="/volume1/mount/home/www/ustb-log"
-LOG_FILE="$SCRIPT_HOME/manage.log"
+# functions
 
 write_log() {
         printf "[%s] %s\n" "$(date +"%Y-%m-%d %H:%M:%S")" "$1" | tee -a $LOG_FILE
-        rsync -az $LOG_FILE $WWW_DIR/
+        rsync -az $LOG_FILE $WWW_DIR/ >/dev/null 2>&1
+        return 0
 }
 
 read_from_cli(){
@@ -22,23 +34,24 @@ read_from_cli(){
 
 create_database(){
     # add content files
-     mkdir -p $HASS_HOME/ustb-daily-report/data/$USER_NAME-$USER_LOC
+    mkdir -p $HASS_HOME/ustb-daily-report/data/$USER_NAME/$USER_LOC
+    mkdir -p $HASS_HOME/ustb-daily-report/data/$USER_NAME/log
 }
 
 update_database(){
+    create_database
     # put data into files
-    echo $COOKIE > $HASS_HOME/ustb-daily-report/data/$USER_NAME-$USER_LOC/REPORT-COOKIE
-    echo $USER_AGENT > $HASS_HOME/ustb-daily-report/data/$USER_NAME-$USER_LOC/REPORT-UA
-    echo $DATA > $HASS_HOME/ustb-daily-report/data/$USER_NAME-$USER_LOC/REPORT-DATA
+    echo $COOKIE > $HASS_HOME/ustb-daily-report/data/$USER_NAME/$USER_LOC/REPORT-COOKIE
+    echo $USER_AGENT > $HASS_HOME/ustb-daily-report/data/$USER_NAME/$USER_LOC/REPORT-UA
+    echo $DATA > $HASS_HOME/ustb-daily-report/data/$USER_NAME/$USER_LOC/REPORT-DATA
 }
 
-# ping test
-test_user(){
+test_data(){
     bash $REPORT_APP test $USER_NAME $USER_LOC > /dev/null
     if [ $? -ne 0 ]
     then
         write_log "Configuration test failed!"
-        rm -rf $HASS_HOME/ustb-daily-report/data/$USER_NAME-$USER_LOC
+        rm -rf $HASS_HOME/ustb-daily-report/data/$USER_NAME/$USER_LOC
         rm -f $HASS_HOME/ustb-daily-report/log/$USER_NAME.log
         exit -1
     else
@@ -47,63 +60,128 @@ test_user(){
     fi
 }
 
-hass_add_user(){
-    # add shell commands
-    cat $HASS_HOME/template-conf.yaml | sed "s/template/$USER_NAME/g" >> $HASS_HOME/configuration.yaml
-    # add report automation
-    cat $HASS_HOME/template-auto.yaml | sed "s/template/$USER_NAME/g" >> $HASS_HOME/automations.yaml
-    # add keep-alive automation
-    sed -i "/- service: shell_command.ustb_ping_yzl/a\  - service: shell_command.ustb_ping_$USER_NAME" $HASS_HOME/automations.yaml
+hass_check_location_existance(){
+    grep "\  - name: $USER_LOC" $HASS_HOME/configuration.yaml >/dev/null && return 1
+    return 0
+}
+
+hass_add_location(){
+    write_log "Adding zone $USER_LOC into hass."
+    sed -i '/^zone:/r template-zone.yaml' $HASS_HOME/configuration.yaml
+    sed -i "s/template-loc/$USER_LOC/g" $HASS_HOME/configuration.yaml
+    sed -i "s/template-lati/$USER_LATI/g" $HASS_HOME/configuration.yaml
+    sed -i "s/template-long/$USER_LONG/g" $HASS_HOME/configuration.yaml
+}
+
+hass_restart(){
     # restart docker to enable automatations
     write_log "Restarting HASS to enable new configuration."
     /usr/local/bin/docker restart hass
 }
 
+hass_check_ping_exist(){
+    grep "shell_command.ustb_ping_$USER_NAME" $HASS_HOME/automations.yaml >/dev/null 2>&1
+}
 
-check_existance(){
-    if [[ -d $HASS_HOME/ustb-daily-report/data/$USER_NAME-$USER_LOC ]]
+hass_auto_add_keepalive(){
+    hass_check_ping_exist && return 0
+    sed -i "$KEEP_ALIVE_LINE i\  - service: shell_command.ustb_ping_$USER_NAME" $HASS_HOME/automations.yaml
+}
+
+hass_auto_add_report(){
+    sed -i "\$r $HASS_HOME/template-auto.yaml" $HASS_HOME/automations.yaml
+    sed -i "s/template-name/$USER_NAME/g" $HASS_HOME/automations.yaml
+    sed -i "s/template-loc/$USER_LOC/g" $HASS_HOME/automations.yaml
+}
+
+hass_auto_update_keepalive(){
+    # remove exsistant ping function
+    sed -i "/ustb_ping_$USER_NAME/d" $HASS_HOME/configuration.yaml
+    # add shell commands
+    sed -i "\$r $HASS_HOME/template-conf.yaml" $HASS_HOME/configuration.yaml
+    sed -i "s/template-name/$USER_NAME/g" $HASS_HOME/configuration.yaml
+    sed -i "/ustb_report_$USER_NAME_template-loc/d" $HASS_HOME/configuration.yaml
+}
+
+hass_conf_add_ping_report(){
+    sed -i "/ustb_ping_$USER_NAME/d" $HASS_HOME/configuration.yaml
+    sed -i "\$r $HASS_HOME/template-conf.yaml" $HASS_HOME/configuration.yaml
+    sed -i "s/template-name/$USER_NAME/g" $HASS_HOME/configuration.yaml
+    sed -i "s/template-loc/$USER_LOC/g" $HASS_HOME/configuration.yaml
+}
+
+hass_auto_add_ping_report(){
+    hass_auto_add_report
+    hass_auto_add_keepalive
+}
+
+hass_add_new(){
+    hass_conf_add_ping_report
+    hass_auto_add_ping_report
+}
+
+hass_update_exist(){
+    hass_auto_update_keepalive
+}
+
+check_user_existance(){
+    grep -Rn "\"username\": \"$USER_NAME\"" $HASS_HOME/.storage/ >/dev/null && return 1
+    write_log "User Not exist, contact admin for a user." && exit -1
+}
+
+check_user_loc_data_existance(){
+    if [[ -d $HASS_HOME/ustb-daily-report/data/$USER_NAME/$USER_LOC ]]
     then
-        write_log "Existed user! Update data by clicking bupdate button."
+        write_log "Existed user+location! Update data by clicking bupdate button."
         exit -1
     fi
 }
 
-check_none_existance(){
-    if [[ ! -d $HASS_HOME/ustb-daily-report/data/$USER_NAME-$USER_LOC ]]
-    then
-        write_log "User Not exist, contact admin for a user."
-        exit -1
-    fi
+chekc_user_current_loc(){
+    grep "ustb_ping_$USER_NAME" $HASS_HOME/configuration.yaml | awk '{print $9}'
 }
 
-# collect data
-if [ "x$1" == "xadd" ]
-then
-    USER_NAME=$2
-    USER_LOC="bj"
-    USER_AGENT=$3
-    COOKIE=$4
-    DATA=$5
-    check_existance
-    create_database
+case $INPUT_CMD in
+add)
+    # check user / location info, failing leads to exit
+    check_user_existance
+    check_user_loc_data_existance
+    
+    # write into database and test user
     update_database
-    test_user $1
-    hass_add_user
-elif [ "x$1" == "xupdate" ]
-then
-    USER_NAME=$2
-    USER_LOC="bj"
-    USER_AGENT=$3
-    COOKIE=$4
-    DATA=$5
-    check_none_existance
+    # failing test leads to exit
+    test_data "add"
+    
+    # if location does net exist, create one
+    hass_check_location_existance && [[ $? -eq 0 ]] && hass_add_location
+    
+    # add conf & auto, ping & report
+    hass_add_new
+    
+    # restart hass to enable update
+    hass_restart
+;;
+update)
+    #check user info, update data, test user
+    check_user_existance
     update_database
-    test_user $1
-else
+    test_data "update"
+    # check current location, if not same to the update, replace the ping function
+    chekc_user_current_loc && [[ "x$?" != "x$USER_LOC" ]] && hass_update_exist && hass_restart
+    
+;;
+remove)
+    rm -rf $HASS_HOME/ustb-daily-report/data/$USER_NAME
+    sed -i "/alias: ustb-daily-report-$USER_NAME/,+11d" $HASS_HOME/automations.yaml
+    sed -i "/shell_command.ustb_ping_$USER_NAME/d" $HASS_HOME/automations.yaml
+    sed -i "/ustb_ping_$USER_NAME/d" $HASS_HOME/configuration.yaml
+    sed -i "/ustb_report_$USER_NAME/d" $HASS_HOME/configuration.yaml
+    hass_restart
+;;
+*)
     read_from_cli
-    check_existance
-    create_database
+    check_user_loc_data_existance
     update_database
-    test_user "add"
-    hass_add_user
-fi
+    test_data "add"
+    hass_auto_add_ping_report
+esac
--- ustb-daily-report/ustb.php
+++ ustb-daily-report/ustb.php
@@ -12,7 +12,9 @@
     <br>
     <form method="POST">
     user name:<br><input type="text" name="user_name" placeholder="Your Home Assistant User"/><br>
-    location:<br><input type="text" name="user_loc" placeholder="UNDERCONSTRUCTION"/><br>
+    location:<br><input type="text" name="user_loc" value="bj"/><br>
+    latitude:<br><input type="text" name="user_lati" value="39.9887"/><br>
+    longitude:<br><input type="text" name="user_long" value="116.3533"/><br>
     user agent:<br>
     <textarea name="user_agent" rows="5" cols="80" placeholder="Your User Agent, e.g., Mozilla/5.0 (Linux; Android 11; SM-G9810 Build/RP1A.200720.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.99 XWEB/3171 MMWEBSDK/20211001 Mobile Safari/537.36 MMWEBID/7956 MicroMessenger/8.0.16.2040(0x2800105B) Process/toolsmp WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64" /></textarea><br>
     user cookie:<br>
@@ -71,16 +73,17 @@
             $user_cookie = trim($user_cookie);
             $user_data = trim($user_data);
             $bash_cmd = "bash /volume1/docker/hass/ustb-daily-report/add-user.sh update '$user_name' '$user_agent' '$user_cookie' '$user_data'";
-            // echo $bash_cmd;
-            // system("ssh terrance@192.168.0.2 \"$bash_cmd\"");
-            exec("ssh terrance@192.168.0.2 \"$bash_cmd\"", $array);
-            print_r($array[0]);
+            file_put_contents("./ustb-log/cmd-update.log", date("Y-m-d H:i:s")." $bash_cmd \n", FILE_APPEND);
+            system("ssh terrance@192.168.0.2 \"$bash_cmd\"");
         }
         if(isset($_POST['submit'])) {
             $user_name=$_POST["user_name"]; 
             $user_agent=$_POST["user_agent"]; 
             $user_cookie=$_POST["user_cookie"]; 
             $user_data=$_POST["user_data"]; 
+            $user_loc=$_POST["user_loc"]; 
+            $user_lati=$_POST["user_lati"]; 
+            $user_long=$_POST["user_long"]; 
 
             if(empty($user_name)) {echo "user name can not be empty"; return;}
             if(empty($user_agent)) {echo "user agent can not be empty"; return;}
@@ -96,11 +99,13 @@
             $user_agent = trim($user_agent);
             $user_cookie = trim($user_cookie);
             $user_data = trim($user_data);
-            $bash_cmd = "bash /volume1/docker/hass/ustb-daily-report/add-user.sh add '$user_name' '$user_agent' '$user_cookie' '$user_data'";
-            // echo $bash_cmd;
-            // system("ssh terrance@192.168.0.2 \"$bash_cmd\"");
-            exec("ssh terrance@192.168.0.2 \"$bash_cmd\"", $array);
-            print_r($array[0]);
+            $user_loc = trim($user_loc);
+            $user_lati = trim($user_lati);
+            $user_long = trim($user_long);
+
+            $bash_cmd = "bash /volume1/docker/hass/ustb-daily-report/add-user.sh \"add\" \"$user_name\" \"$user_agent\" \"$user_cookie\" \"$user_data\" \"$user_loc\" \"$user_lati\" \"$user_long\"";
+            file_put_contents("./ustb-log/cmd-add.log", date("Y-m-d H:i:s")." $bash_cmd \n", FILE_APPEND);
+            system("ssh terrance@192.168.0.2 \"$bash_cmd\"");
         }
         
     ?>
